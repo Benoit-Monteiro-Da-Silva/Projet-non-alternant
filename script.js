a7b6eabfe2859d5781a21c129b037e6c151094d7
Benoit-Monteiro-Da-Silva <benoit.monteirods@gmail.com>
2024-12-12 15:34:16 +0100

    landing page complete !

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const searchButton = document.querySelector(".search-button")
 const connexionButton = document.querySelector(".connexion-button")
 const closeConnexionModalButton = document.querySelector("#close-connexion-modal-button")
 const closeSearchModalButton = document.querySelector("#close-search-modal-button")
+const filtersListContainer = document.querySelector('.filters-list-container')
 const filtersList = document.querySelector('.filters-list')
 const filters = document.querySelectorAll('.filters-list button')
 const arrowButtons = document.querySelectorAll('.arrow-button')
 
-const connexionModalToggle = () => {
-    connexionModal.classList.toggle('open')
-    body.classList.toggle('modal-opened')
-}
-
-const searchModalToggle = () => {
+const searchModalToggle = (e) => {
+    e.preventDefault()
     if (window.innerWidth < 768) {
         searchModal.classList.toggle('open')
         body.classList.toggle('modal-opened')
+    }
+}
+
+const connexionModalToggle = () => {
+    connexionModal.classList.toggle('open')
+    if (window.innerWidth < 768) {
+        body.classList.toggle('modal-opened')
+    } else {
+        connexionButton.classList.toggle('modal-opened')
     }
 }
 
@@ -35,6 +41,12 @@ const filtersScrollLeft = () => {
         left: filtersList.scrollLeft - 300,
         behavior: 'smooth'
     })
+    if (filtersList.scrollLeft - 300 < 50) {
+        filtersListContainer.style.setProperty('--before-display', 'none')
+    }
+    if (filtersList.scrollWidth - filtersList.clientWidth > 50) {
+        filtersListContainer.style.setProperty('--after-display', 'block')
+    }
 }
 
 const filtersScrollRight = () => {
@@ -42,6 +54,12 @@ const filtersScrollRight = () => {
         left: filtersList.scrollLeft + 300,
         behavior: 'smooth'
     })
+    if (filtersList.scrollLeft + 300 > filtersList.scrollWidth - filtersList.clientWidth - 50) {
+        filtersListContainer.style.setProperty('--after-display', 'none')
+    }
+    if (filtersList.scrollWidth - filtersList.clientWidth > 50) {
+        filtersListContainer.style.setProperty('--before-display', 'block')
+    }
 }
 
 connexionButton.addEventListener("click", connexionModalToggle)
@@ -49,6 +67,6 @@ searchButton.addEventListener("click", searchModalToggle)
 closeSearchModalButton.addEventListener("click", searchModalToggle)
 closeConnexionModalButton.addEventListener("click", connexionModalToggle)
 
-filters.forEach((filter) => {filter.addEventListener("click", (e) => {filterToggle(e)})})
+filters.forEach(filter => filter.addEventListener("click", filterToggle))
 arrowButtons[0].addEventListener("click", filtersScrollLeft)
 arrowButtons[1].addEventListener("click", filtersScrollRight)
